82084fe85d74f1b89a94a9d5d6edf9e8c528bf18
Akshay Jagtiani <ajagtian@usc.edu>
2015-02-23 18:55:13 -0800

    update perceptron scripts

--- percepclassify.py
+++ percepclassify.py
@@ -6,15 +6,21 @@ import re
 
 def	get_g_hash_from_file(model_file_name):
 		f = open(model_file_name, 'rU', errors = 'ignore')
-		text = f.read()
+		lines = f.readlines()
 		f.close()
-		g_hash = ast.literal_eval(text)
-
+		g_hash = {}
+		clazz_count = int(lines[0].strip())
+		lines = lines[1:]
+		for line in lines:
+			clazz = line[:line.find('~')]
+			clazz_hash = ast.literal_eval(line[line.find('~')+1:])
+			g_hash[clazz] = clazz_hash
+		
 		return g_hash
 
 
 def	max_score(score_hash):
-		max = 0
+		max = float('-Inf')
 		max_class = ''
 		for clazz in score_hash.keys():
 			if score_hash[clazz] >= max:
@@ -26,12 +32,11 @@ def	max_score(score_hash):
 
 def	classify(text, g_hash):
 		score_hash = {}
-		words = re.findall(r'(\w+)', text)
+		words = re.findall(r'(\S+)', text)
 		for clazz in g_hash.keys():
 			clazz_hash = g_hash[clazz]
 			score = 0
 			for word in words:
-				word = word.lower()
 				if clazz_hash.get(word):
 					score += clazz_hash[word]
 			score_hash[clazz] = score
@@ -43,12 +48,12 @@ def	classify(text, g_hash):
 
 def	main():
 		args = sys.argv[1:]
-		if(len(args) != 2):
-			print('usage: ./percepclassify <model_file_name> <test_input>')
+		if(len(args) != 1):
+			print('usage: ./percepclassify <model_file_name>')
 			exit(0)
-
+		test_text = input()
 		g_hash = get_g_hash_from_file(args[0])
-		print(classify(args[1], g_hash))
+		print(classify(test_text, g_hash))
 
 
 if __name__ == '__main__':
--- perceplearn.py
+++ perceplearn.py
@@ -21,11 +21,11 @@ def	extract_classes_and_mine_words_from_training_file(filename):
 
 
 def	index_file(text, word_count, word_hash):
-		words = re.findall(r'(\w+)', text)
+		words = re.findall(r'(\S+)', text)
 		
 		for word in words:
-			if word.lower() not in word_hash.keys():
-				word_hash[word.lower()] = word_count
+			if word not in word_hash.keys():
+				word_hash[word] = word_count
 				word_count += 1
 		return (word_hash, word_count)
 
@@ -43,7 +43,7 @@ def 	init_hashes(classes, words):
 					
 
 def	max_score(score_hash):
-		max = 0
+		max = float('-Inf')
 		max_class = ''
 		for clazz in score_hash.keys():
 			if score_hash[clazz] >= max:
@@ -55,46 +55,55 @@ def	max_score(score_hash):
 
 def	update_g_hash_with_a_file(original_class, text, g_hash):
 		score_hash = {}
-		words = re.findall(r'(\w+)', text)
+		words = re.findall(r'(\S+)', text)
 		for clazz in g_hash.keys():
 			score = 0
 			clazz_hash = g_hash[clazz]
 			for word in words:
-				word = word.lower()
 				score += clazz_hash[word]
 			score_hash[clazz] = score
 		
 		predicted_class = max_score(score_hash)
-		
+		accuracy_adddendum = 0	
 		if not predicted_class.strip() == original_class.strip():
 			for clazz in g_hash.keys():
 				clazz_hash = g_hash[clazz]
 				if clazz == original_class:
 					for word in words:
-						word = word.lower()
 						clazz_hash[word] += 1
 				else:
 					for word in words:
-						word = word.lower()
 						clazz_hash[word] -= 1
 				g_hash[clazz] = clazz_hash
-		
-		return g_hash
+	
+		else:
+			accuracy_adddendum = 1
+			
+		return (g_hash, accuracy_adddendum)
 
 
 def	update_g_hash(training_file_name, g_hash):
 		f = open(training_file_name, 'rU', errors = 'ignore')
 		lines = f.readlines()
 		f.close()
+		correct_guesses = 0
+		total_lines = 0
 		for line in lines:
-			g_hash = update_g_hash_with_a_file(line[:line.find(' ')], line[line.find(' ')+1:], g_hash)		
+			(g_hash, accuracy_adddendum) = update_g_hash_with_a_file(line[:line.find(' ')], line[line.find(' ')+1:], g_hash)		
+			correct_guesses += accuracy_adddendum
+			total_lines += 1
+		
 
-		return g_hash
+		return (g_hash, correct_guesses, total_lines)
 
 
 def	write_g_hash_to_file(g_hash, model_file_name):
 		f = open(model_file_name, 'w')
-		f.write(str(g_hash))
+		clazz_count = len(g_hash.keys())
+		f.write(str(clazz_count)+'\n')
+		for clazz in g_hash.keys():
+			clazz_hash = g_hash[clazz]
+			f.write(clazz+'~'+str(clazz_hash)+'\n')
 		f.close()
 
 
@@ -102,7 +111,7 @@ def	main():
 		args = sys.argv[1:]
 		if len(args) != 2:
 			print('usage: ./perceplearn.py <training_file> <mode_file>')
-			exit(0)		
+			sys.exit(0)		
 		print('preprocessing...')
 		(classes, word_hash, word_count) = extract_classes_and_mine_words_from_training_file(args[0])
 		print('initializing model...')
@@ -110,9 +119,11 @@ def	main():
 		#loop start
 		print('training...')
 		for i in range(30):
-			print('. ')
-			g_hash = update_g_hash(args[0], g_hash)
-		print('trained by 30 iterations')
+			(g_hash, correct_guesses, total_lines) = update_g_hash(args[0], g_hash)
+			print('... accuracy '+str(correct_guesses/total_lines))
+			if correct_guesses/total_lines > .99:
+				break
+		print('trained by '+str(i+1)+' iterations')
 		# loop end
 		print('saving model to file '+args[1]+' ...')
 		write_g_hash_to_file(g_hash, args[1])
